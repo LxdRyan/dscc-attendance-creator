5871fbc4f8521ca3e0be1058da4cd7adeac09162
Ryan <ryanxdlee@gmail.com>
2024-08-14 21:14:52 +0800

    move a bit of stuff around

--- src/attendance/__main__.py
+++ src/attendance/__main__.py
@@ -2,8 +2,7 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 import click
 import contextlib
 import os
-import time
-
+from .get import get_date, get_time, get_course, get_type
 
 @click.command
 def cli():
@@ -12,60 +11,6 @@ def cli():
     )
     attendance = env.get_template("attendance_template.jinja")
     end = env.get_template("end_template.jinja")
-
-    def get_date():
-        date = input("Date (DDMMYY): ")
-        try:
-            time.strptime(date, "%d%m%y")
-        except ValueError as e:
-            print("Date must be in DDMMYY format", e)
-            return get_date()
-        else:
-            return date
-
-    def get_time():
-        lesson_time = input("Time (HHMM): ")
-        try:
-            time.strptime(lesson_time, "%H%M")
-        except ValueError:
-            print("Time must be in HHMM format")
-            return get_time()
-        else:
-            return lesson_time
-
-    def get_course():
-        courses = {
-            "SC1003": "Introduction to Computational Thinking & Programming",
-            "SC1004": "Linear Algebra for Computing",
-            "SC1005": "Digital Logic",
-            "SC1013": "Physics For Computing",
-            "EG1001": "Engineers in Society",
-            "CC0003": "Ethics & Civics in a Multicultural World",
-            "CC0005": "Healthy Living & Wellbeing",
-        }
-        code = input("Course Code: ")
-        try:
-            code = code.upper()
-            name = courses[code]
-        except KeyError:
-            print(
-                f"{code} is not a valid course code\nAvailable courses: {", ".join(courses.keys())}"
-            )
-            get_course()
-        return code, name
-
-    def get_type():
-        lesson_types = {"lec": "lecture", "tut": "tutorial", "lab": "lab"}
-        lesson_type = input("Type (Lecture, Tutorial, Lab): ").lower()
-        if lesson_type in lesson_types:
-            return lesson_types[lesson_type].title()
-        elif lesson_type in lesson_types.values():
-            return lesson_type.title()
-        else:
-            print(
-                f"{lesson_type} is not a valid type\nAvailable types: {", ".join(lesson_types.keys())}"
-            )
-            get_type()
 
     date = get_date()
     lesson_time = get_time()
@@ -76,7 +21,7 @@ def cli():
     people = list(
         map(
             lambda x: f"PTE {x}",
-            input("People (semicolon separated): ").split("; "),
+            input("People (semicolon separated): ").split("; ").split(";"),
         )
     )
     video = input("Attendance Video URL: ")
@@ -97,3 +42,7 @@ def cli():
                 people=people,
                 video=video,)
             )
+
+
+if __name__ == "__main__":
+    cli()
