8d9339dd5b8eb2ee67ee2b435f31e8da4e0e8bf3
Ryan <ryanxdlee@gmail.com>
2024-08-14 23:01:52 +0800

    wooooooooooo config works

--- src/attendance/__main__.py
+++ src/attendance/__main__.py
@@ -2,7 +2,8 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 import click
 import contextlib
 import os
-from .config import load_config
+from re import split as re_split
+from .config import load_config, edit_config
 from .get import get_date, get_time, get_course, get_type
 
 
@@ -10,7 +11,10 @@ from .get import get_date, get_time, get_course, get_type
 def cli():
     pass
 
-@cli.command
+
+@cli.command(
+    help="Creates attendance messages for start and end of lessons (stored in the folder output)"
+)
 def create():
     env = Environment(
         loader=PackageLoader("attendance"), autoescape=select_autoescape()
@@ -30,16 +34,21 @@ def create():
     people = list(
         map(
             lambda x: f"{config.rank} {x}",
-            input("People (semicolon separated): ").split("; ").split(";"),
+            re_split(r";\s*", input("People (semicolon separated): ")),
         )
     )
     video = input("Attendance Video URL: ")
 
     with contextlib.suppress(FileExistsError):
-        os.mkdir("output")
+        os.mkdir(f"{config.output_folder}")
+    
+    with contextlib.suppress(FileExistsError):
+        os.mkdir(f"{config.output_folder}/{course[0]}_{date}_{start_time}")
 
     for name, template in {"attendance": attendance, "end": end}.items():
-        with open(f"output/{name}_{course[0]}_{date}_{start_time}.txt", "w") as f:
+        with open(
+            f"{config.output_folder}/{course[0]}_{date}_{start_time}/{name}.txt", "w"
+        ) as f:
             f.write(
                 template.render(
                     date=date,
@@ -52,9 +61,27 @@ def create():
                     ic=ic,
                     people=people,
                     video=video,
-                    folder=config.folder,
+                    drive_folder=config.drive_folder,
                 )
             )
+
+
+@cli.command(
+    help="Updates config values (Rank, Batch, Google Drive folder) for all subsequent uses"
+)
+@click.option("--output-folder", help="Folder which messages will be output to")
+@click.option("--rank", help="Rank of cadets")
+@click.option("--batch", help="DSCC Batch")
+@click.option("--drive-folder", help="Link to Google Drive folder of attendance videos")
+def config(output_folder, rank, batch, drive_folder):
+    if output_folder:
+        edit_config(output_folder=output_folder)
+    if rank:
+        edit_config(rank=rank)
+    if batch:
+        edit_config(batch=batch)
+    if drive_folder:
+        edit_config(drive_folder=drive_folder)
 
 
 if __name__ == "__main__":
--- src/attendance/config.py
+++ src/attendance/config.py
@@ -9,18 +9,21 @@ class ConfigDict(dict):
         if item in self:
             return self[item]
 
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __delattr__(self, item):
-        if item in self:
-            del self[item]
-
 
 def load_config():
     with config_file.open("r") as f:
-        config = json.load(f)
-    return ConfigDict(config)
+        config = ConfigDict(json.load(f))
+    return config
+
 
 def edit_config(**kwargs):
-    
+    with config_file.open("r") as f:
+        config = ConfigDict(json.load(f))
+        config.update(kwargs)
+    with config_file.open("w") as f:
+        f.write(json.dumps(config, indent=4))
+    print(
+        ", ".join(
+            [f"{k.replace("_", " ").title()} updated to {v}" for k, v in kwargs.items()]
+        )
+    )
